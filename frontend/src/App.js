656a2ee421cc2a9e4d4b8bafe4de45d96403255b
alwinsDen <jsmadesimple@gmail.com>
2021-09-09 09:38:23 +0530

    added music

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,17 +8,28 @@ import {RightNav} from "./right navigation bar/RightNav"
 import {Module2} from "./module2/Module2"
 import {Module3} from "./module3/Module3"
 import backVid from "./wallpapers/video.mp4"
+import backMusic from "./wallpapers/backMusic.mp3" 
 function App() {
+  const autoMusic=()=>{
+    document.getElementById("backMusic").play();
+  }
   useEffect(()=>
   {
-    document.getElementById("backVid1").play();
-  })  
+    document.getElementById("backVid1").play(); 
+    window.addEventListener("mouseover",autoMusic,false);
+    return()=>{
+      window.removeEventListener("mouseover",autoMusic)
+    }
+  },[])  
   return (
-    <div className="App"
+    <div className="App" id="App"
     >
       <video autoplay muted loop id="backVid1">
         <source src={backVid} type="video/mp4"/>
       </video>
+      <audio id="backMusic" loop hidden>
+        <source src={backMusic} type="audio/mpeg"/>
+      </audio>
       <div className="subApp">
         <LeftNav />
         <RightNav />
